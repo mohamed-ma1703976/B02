faa606fe64bbfbfdbec86cfa2a106fcf3f17064c
WaleedBinOwais <64152361+WaleedBinOwais@users.noreply.github.com>
2021-10-12 12:43:21 +0300

    l9

---
+++ Lab9/BankingApp/app/src/main/java/cmps312/bankingapp/webapi/BankService.kt
@@ -0,0 +1,6 @@
+package cmps312.bankingapp.webapi
+
+import cmps312.bankingapp.model.Account
+import cmps312.bankingapp.model.Beneficiary
+import cmps312.bankingapp.model.Transfer
+
---
+++ Lab9/BankingApp/app/src/main/java/cmps312/bankingapp/webapi/TestBankService.kt
@@ -0,0 +1,7 @@
+package cmps312.bankingapp.webapi
+
+import cmps312.bankingapp.model.Transfer
+import kotlinx.coroutines.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
